180affc57033cb34fc4bc60ef0cbe3486efcecc0
Karl Wessel <karl.wessel@stud.uni-goettingen.de>
2020-12-24 12:32:12 +0100

    explicit gouraud shading parameter is not supported any more

--- tests/test_meshes.py
+++ tests/test_meshes.py
@@ -9,29 +9,6 @@ from matplotlib.testing.decorators import image_comparison
 
 pytestmark = pytest.mark.backend('module://mplopengl.backend_qtgl')
 
-
-@image_comparison(baseline_images=['quadmesh_example'],
-                  extensions=['png'])
-def test_quadmesh_example():
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
-
-    n = 12
-    x = np.linspace(-1.5, 1.5, n)
-    y = np.linspace(-1.5, 1.5, n * 2)
-    X, Y = np.meshgrid(x, y)
-    Qx = np.cos(Y) - np.cos(X)
-    Qz = np.sin(Y) + np.sin(X)
-    Z = np.sqrt(X ** 2 + Y ** 2) / 5
-    Z = (Z - Z.min()) / (Z.max() - Z.min())
-
-    # The color array can include masked values.
-    Zm = np.ma.masked_where(np.abs(Qz) < 0.5 * np.max(Qz), Z)
-
-    ax.pcolormesh(Qx, Qz, Z)
-    ax.set_title('Without masked values')
-
-
 @image_comparison(baseline_images=['gouradtri_example'],
                   extensions=['png'])
 def test_gouradtri_example():
